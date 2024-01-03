e4e352dedb3de9fb5d914429618ec4880c19f17d
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-01-04 00:01:19 +0300

    Completed task

--- src/Components/CategoryDropdown.js
+++ src/Components/CategoryDropdown.js
@@ -4,54 +4,66 @@ import JokeDisplay from './JokeDisplay';
 import { useState, useEffect } from 'react';
 
 function CategoryDropdown() {
-  // const [selected, setSelected]= useState(null)
-  const url= `https://v2.jokeapi.dev/joke/Programming`
-  const [loading, setLoading]= useState(true)
-  const [error, setError]= useState(null)
+  const [selected, setSelected]= useState(null)
+  const url= `https://v2.jokeapi.dev/joke/${selected}`
+  // const [loading, setLoading]= useState(true)
+  // const [error, setError]= useState(null)
   const [data,setData]= useState([])
 
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const response = await fetch(url);
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok');
+  //       }
 
-        const data = await response.json();
-        setData(data)
-        // console.log(data)
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    }
+  //       const data = await response.json();
+  //       setData(data)
+  //       // console.log(data)
+  //     } catch (error) {
+  //       setError(error.message);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   }
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
-  if (loading) {
-    return <p>Loading...</p>;
+  // if (loading) {
+  //   return <p>Loading...</p>;
+  // }
+
+  // if (error) {
+  //   return <p>Error: {error}</p>;
+  // }
+
+  const handleCategoryChange = (event) => {
+    setSelected(event.target.value);
   }
 
-  if (error) {
-    return <p>Error: {error}</p>;
+  const handleSubmit= async()=> {
+    const response = await fetch(`https://v2.jokeapi.dev/joke/${selected}`);
+    const result = await response.json();
+    setData(result);
   }
+
 
   return (
     <div>
-      <Form.Select aria-label="Default select example" className=''>
+      <Form.Select onChange={handleCategoryChange} value={selected} aria-label="Default select example" className=''>
         <option>Select joke category</option>
-        <option value="1">Programming</option>
-        <option value="2">Misc</option>
-        <option value="3">Dark</option>
-        <option value="3">Pun</option>
-        <option value="3">Spooky</option>
+        <option value="Programming">Programming</option>
+        <option value="Miscellaneous">Miscellaneous</option>
+        <option value="Dark">Dark</option>
+        <option value="Pun">Pun</option>
+        <option value="Spooky">Spooky</option>
+        <option value="Christmas"> Christmas</option>
       </Form.Select>
       <br/>
-      <Button>Get a joke!</Button>
+      <Button onClick={handleSubmit}>Get a joke!</Button>
       <br/>
       <br/>
       <JokeDisplay joke= {data.joke} setup= {data.setup} delivery= {data.delivery}/>
