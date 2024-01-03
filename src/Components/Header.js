0eb1c518e6c46463476d2d257dcaeb08267ca192
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-01-03 16:35:13 +0300

    Header and Styles

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+        <div>
+            <h1 className="text-center text-4xl font-bold mt-8">MyJoke</h1>
+            <br/>
+            <p className='para'>Can I tell you a Joke?</p>
+        </div>
+    </div>
   )
 }
 
