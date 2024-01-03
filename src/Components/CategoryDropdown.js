b29c2c8652db9704db3a857e5d1acde0f46b1021
dmmuchoki7 <dmmuchoki7@gmail.com>
2024-01-03 15:01:55 +0300

    Components stucture

---
+++ src/Components/CategoryDropdown.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CategoryDropdown() {
+  return (
+    <div>CategoryDropdown</div>
+  )
+}
+
+export default CategoryDropdown
---
+++ src/Components/JokeAPI.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function JokeAPI() {
+  return (
+    <div>JokeAPI</div>
+  )
+}
+
+export default JokeAPI
